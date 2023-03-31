ed5166e53bbda03ee60a1813d03f8baf2b8f1ba8
SergeyGSA <77gsa88gmail.com>
2023-03-31 18:59:52 +0300

    1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,49 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
+  message: string;
+};
+
+interface IError {
+  error: { message: string };
 }
 
-export default function handler(
+const TG_SEND_MESSAGE_URL = `https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage`;
+
+export default function formHandler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | IError>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.body);
+
+  if (req.method === 'POST') {
+  }
+
+  try {
+    // fetch(TG_SEND_MESSAGE_URL, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     chat_id: process.env.TG_CHAT_ID,
+    //     text: req.body,
+    //     parse_mode: 'html',
+    //   }),
+    // })
+
+    // .then((res) => console.log(res))
+    // .catch((err) => console.error(err));
+
+    // axios.post(TG_SEND_MESSAGE_URL, {
+    //   chat_id: process.env.CHAT_ID,
+    //   text: req.body,
+    //   parse_mode: 'html',
+    // });
+
+    res.status(200).json({ message: 'Ok' });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 }
