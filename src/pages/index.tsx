8a8c315f5663641dbb3e442790d08f78226b6a9e
SergeyGSA <77gsa88gmail.com>
2023-04-04 23:18:48 +0300

    footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import {
   Tech,
   Works,
 } from '@/components'
+import { styles } from '@/styles/styles'
 
 export default function Home() {
   return (
@@ -37,6 +38,16 @@ export default function Home() {
             <StarsCanvas />
           </div>
         </main>
+        <footer className="grid place-items-center bg-primary py-5">
+          <div className="flex">
+            <span className={styles.sectionSubText}>
+              {new Date().getFullYear().toString()}&nbsp;&nbsp;&nbsp;
+            </span>
+            <p className={styles.sectionSubText}>
+              <span>&#169;</span>&nbsp;copyright
+            </p>
+          </div>
+        </footer>
       </div>
     </>
   )
