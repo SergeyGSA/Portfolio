e7d339aa4f93d2139522dfcd313be74fdd1ff95d
SergeyGSA <77gsa88gmail.com>
2023-04-05 12:53:17 +0300

    added formik to form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,14 @@
-import React, { ChangeEvent, FormEvent, RefObject } from 'react'
-import { useState, useRef } from 'react'
+import React from 'react'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
+import axios from 'axios'
+import { Formik, Form, ErrorMessage, Field } from 'formik'
 
 import { styles } from '@/styles/styles'
 import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
+import { messageSchema } from '@/validation'
 
 interface IForm {
   name: string
@@ -13,40 +16,30 @@ interface IForm {
   message: string
 }
 
+const API_URL = `http://localhost:3000/api/hello`
+
 const Contact: React.FC = () => {
-  const formRef: RefObject<HTMLFormElement> = useRef<HTMLFormElement>(null)
-  const [form, setForm] = useState<IForm>({
+  const initialValues: IForm = {
     name: '',
     email: '',
     message: '',
-  })
-  const [loading, setLoading] = useState<boolean>(false)
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-    const { name, value } = e.target
-
-    setForm({ ...form, [name]: value })
   }
 
-  const handleSubmit = (e: FormEvent): void => {
-    e.preventDefault()
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const handleSubmit = async (
+    values: IForm,
+    { resetForm }: { resetForm: () => void },
+  ): Promise<void> => {
     setLoading(true)
 
-    const formData: IForm = {
-      name: form.name,
-      email: form.email,
-      message: form.message,
-    }
+    await axios
+      .post(API_URL, values)
+      .then((res: any) => console.log(res))
+      .catch((err: any) => console.log(err))
 
-    console.log(formData)
-
-    setTimeout(() => setLoading(false), 3000)
-
-    setForm({
-      name: '',
-      email: '',
-      message: '',
-    })
+    setLoading(false)
+    resetForm()
   }
 
   return (
@@ -57,50 +50,55 @@ const Contact: React.FC = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact</h3>
-        <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your name?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
 
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your email?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={messageSchema}
+        >
+          {() => (
+            <Form>
+              <label className="flex flex-col">
+                <span className="text-white font-medium mb-4">Your Name</span>
+                <Field
+                  name="name"
+                  placeholder="What's your name?"
+                  className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                />
+              </label>
+              <ErrorMessage name="name" component="span" />
 
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What do you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium resize-none"
-            />
-          </label>
+              <label className="flex flex-col">
+                <span className="text-white font-medium mb-4">Your Email</span>
+                <Field
+                  name="email"
+                  placeholder="What's your email?"
+                  className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+                />
+              </label>
+              <ErrorMessage name="email" component="span" />
 
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
-          >
-            {loading ? 'Sending...' : 'Send'}
-          </button>
-        </form>
+              <label className="flex flex-col">
+                <span className="text-white font-medium mb-4">Your Message</span>
+                <Field
+                  as="textarea"
+                  rows={7}
+                  name="message"
+                  placeholder="What do you want to say?"
+                  className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium resize-none"
+                />
+              </label>
+              <ErrorMessage name="message" component="span" />
+
+              <button
+                type="submit"
+                className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+              >
+                {loading ? 'Sending...' : 'Send'}
+              </button>
+            </Form>
+          )}
+        </Formik>
       </motion.div>
 
       <motion.div
